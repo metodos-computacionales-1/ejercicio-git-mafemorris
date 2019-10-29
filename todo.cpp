f5161bb16e635735f387bd01b7a8caeabce7cb37
Mafe Morris <mafemorris99@gmail.com>
2019-10-29 16:42:00 -0500

    Los include y los using stds

--- todo.cpp
+++ todo.cpp
@@ -6,13 +6,25 @@ using std::endl;
 using std::cin;
 
 int main(){
-    double radio;
-    cout << "Ingrese el radio" << endl;
-    cin >> radio;
-    double diametro = 2*radio;
-    double perimetro = 2* M_PI * radio;
-    double area = 2*M_PI*radio * pow(radio,2)  ;
+    int v1;
+    int v2;
+    int v3;
 
-    cout << "Para el radio es "<< radio<<" el diametro es " << diametro << ", el perimetro es "<< perimetro << " y el area " << area << endl;
+    cout << "Ingrese el primer dato" << endl;
+    cin >> v1;
+    cout << "Ingrese el segundo dato" << endl;
+    cin >> v2;
+    cout << "Ingrese el tercer dato" << endl;
+    cin >> v3;
+
+    if(sqrt(v1*v1 + v2*v2)==v3 || sqrt(v1*v1 + v3*v3)==v2 || sqrt(v3*v3 + v2*v2)==v1){
+        cout << "Es triangulo rectangulo" << endl;
+    }
+    else{
+        cout << "No es triangulo rectangulo" << endl;
+    }
+
     return 0;
+
+
 }
