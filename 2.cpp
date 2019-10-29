de1d4f63155dfa249611cf1eae6d12b54a1f4596
Mafe Morris <mafemorris99@gmail.com>
2019-10-29 16:42:47 -0500

    Main, variables e inputs de los lados

--- 2.cpp
+++ 2.cpp
@@ -4,3 +4,22 @@
 using std::cout;
 using std::endl;
 using std::cin;
+
+int main(){
+    int v1;
+    int v2;
+    int v3;
+
+    cout << "Ingrese el primer dato" << endl;
+    cin >> v1;
+    cout << "Ingrese el segundo dato" << endl;
+    cin >> v2;
+    cout << "Ingrese el tercer dato" << endl;
+    cin >> v3;
+
+    
+
+    return 0;
+
+
+}
