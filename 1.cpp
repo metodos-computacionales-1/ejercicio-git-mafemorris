bac191cd9aafe07e424178ae6853a4fa71167d19
Mafe Morris <mafemorris99@gmail.com>
2019-10-29 16:39:22 -0500

    clases y using stds

--- 1.cpp
+++ 1.cpp
@@ -4,15 +4,3 @@
 using std::cout;
 using std::endl;
 using std::cin;
-
-int main(){
-    double radio;
-    cout << "Ingrese el radio" << endl;
-    cin >> radio;
-    double diametro = 2*radio;
-    double perimetro = 2* M_PI * radio;
-    double area = 2*M_PI*radio * pow(radio,2)  ;
-
-    cout << "Para el radio es "<< radio<<" el diametro es " << diametro << ", el perimetro es "<< perimetro << " y el area " << area << endl;
-    return 0;
-}
