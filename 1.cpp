99739d4ca077ec02bb36866cce025d6b10d603cf
Mafe Morris <mafemorris99@gmail.com>
2019-10-29 16:40:20 -0500

    Main y variables

--- 1.cpp
+++ 1.cpp
@@ -4,3 +4,15 @@
 using std::cout;
 using std::endl;
 using std::cin;
+
+int main(){
+    double radio;
+    cout << "Ingrese el radio" << endl;
+    cin >> radio;
+    double diametro = 2*radio;
+    double perimetro = 2* M_PI * radio;
+    double area = 2*M_PI*radio * pow(radio,2)  ;
+
+    
+    return 0;
+}
